76a245d7959a7f9f071984aee46a63eb56371685
TQ <tilekbergen.third@gmail.com>
2024-05-13 10:37:35 -0700

    connected vue to golang

--- main.go
+++ main.go
@@ -5,15 +5,17 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gin-gonic/gin"
 	socketio "github.com/googollee/go-socket.io"
 )
 
 func main() {
+	router := gin.New()
 	server := socketio.NewServer(nil)
 
 	server.OnConnect("/", func(s socketio.Conn) error {
 		// s.SetContext("")
-		fmt.Println("connected:", s.ID())
+		fmt.Println("New connection")
 		return nil
 	})
 
@@ -54,9 +56,38 @@ func main() {
 	}()
 	defer server.Close()
 
-	http.Handle("/socket.io/", server)
-	http.Handle("/", http.FileServer(http.Dir("./asset")))
+	// http.Handle("/socket.io/", server)
+	// http.Handle("/", http.FileServer(http.Dir("./assets")))
 
-	log.Println("Serving at localhost:8000...")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	// log.Println("Serving at localhost:8000...")
+	// log.Fatal(http.ListenAndServe(":8000", nil))
+
+	router.Use(GinMiddleware("http://localhost:5173"))
+	router.GET("/socket.io/*any", gin.WrapH(server))
+	router.POST("/socket.io/*any", gin.WrapH(server))
+	router.Static("/assets", "./assets")
+	router.StaticFS("/more_static", http.Dir("my_file_system"))
+	router.StaticFile("/favicon.ico", "./resources/favicon.ico")
+
+	if err := router.Run(":8000"); err != nil {
+		log.Fatal("failed run app: ", err)
+	}
+}
+
+func GinMiddleware(allowOrigin string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", allowOrigin)
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, Content-Length, X-CSRF-Token, Token, session, Origin, Host, Connection, Accept-Encoding, Accept-Language, X-Requested-With")
+
+		if c.Request.Method == http.MethodOptions {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		c.Request.Header.Del("Origin")
+
+		c.Next()
+	}
 }
